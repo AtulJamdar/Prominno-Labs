bccc35ad972147b30ca5e73d142b7ad5ba6d84ec
AtulJamdar <work.atuljamdar@gmail.com>
2026-05-29 18:46:50 +0530

    images issue occurred

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import Button from "../ui/Button";
-import heroImage from "../../assets/hero.jpg";
 
 export default function Hero() {
   return (
@@ -59,7 +58,7 @@ export default function Hero() {
           >
             {/* Image */}
             <img
-              src={heroImage}
+              src="https://images.unsplash.com/photo-1498050108023-c5249f4df085"
               alt="Hero"
               className="w-full h-full object-cover"
             />
